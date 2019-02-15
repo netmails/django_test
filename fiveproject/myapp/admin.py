c8cb710ec59ef6665e88bc3b2ba56e362a641956
root <root@localhost.localdomain>
2019-02-15 15:02:45 +0530

    first commit

---
+++ fiveproject/myapp/admin.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+from myapp.models import UserProfileInfo
+
+# Register your models here.
+
+admin.site.register(UserProfileInfo)
